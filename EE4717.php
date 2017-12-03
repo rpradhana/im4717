01c9083c1d6752dc56179e96793c75937df2d9fa
rmonap <raymond.pradhana@gmail.com>
2017-12-03 17:53:24 +0800

    add exam notes

---
+++ EE4717.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<meta charset="utf-8">
+<head>
+	<title>EE4717</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
+	<link rel="icon" type="image/png"  href="./static/images/favicon.png">
+	<style type="text/css">
+
+		/**
+		 * Element styling
+		 */
+		body {}
+
+		/**
+		 * Custom font declaration
+		 */
+		@font-face {
+			font-family: 'Graublau Web';
+			src: url('GraublauWeb.eot?') format('eot'),
+				 url('GraublauWeb.woff') format('woff2'),
+				 url('GraublauWeb.woff') format('woff'),
+				 url('GraublauWeb.ttf') format('truetype');
+		}
+
+		/**
+		 * Classes
+		 */
+		.css {
+			font-family: "Graublau Web", Verdana, Calibri, Arial, sans-serif;
+			background: #CCCCCC;
+			color: #000066;
+			background: #FFFFFF;
+			max-width: 90%;
+			min-width: 900px;
+			padding: 0;
+
+			/* Align center for position: relative*/
+			margin: auto;
+
+			/**
+			 * 4: Top, Right, Bottom, Left
+			 * 3: Top, Right+Left, Bottom
+			 * 2: Top+Bottom, Right+Left
+			 * 1: All sides
+			 */
+			border: 1px 2px 3px 4px solid #0099CC;
+		}
+
+		/**
+		 * Media queries
+		 */
+		@media screen and (max-width: 1280px) and (min-width: 768px){
+			.css {}
+		}
+	</style>
+</head>
+<body>
+	<div id="" class="">
+		<form id=""
+		      name=""
+		      onsubmit="return validate()"
+		      action="newpage.php"
+		      method="">
+			<button type=""></button>
+			<input type=""
+				   name=""
+				   onclick=""
+				   placeholder=""
+				   required>
+			<label for="input-with-label">
+				<input type="checkbox"
+					   name="input-with-label"
+					   value=""
+					   checked>
+			</label>
+		</form>
+		<img src="" alt="">
+		<a href=""></a>
+		<a href="" target="_blank"></a>
+		<a href="tel:+6598765432"></a>
+		<a href="mailto:address@domain.com"></a>
+		<aside></aside>
+		<video poster="" controls>
+			<source src=""
+					type="video/mp4">
+		</video>
+		<audio src="" controls>
+			<source src="" type="audio/ogg">
+		</audio>
+	</div>
+
+	<!-- PHP -->
+
+	<?php
+		define(name, value)
+		session_start();
+
+		$DBServer = 'server name or IP address'; // e.g 'localhost' or '192.168.1.100'
+		$DBUser   = 'DB_USER';
+		$DBPass   = 'DB_PASSWORD';
+		$DBName   = 'DB_NAME';
+		$conn = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
+
+		if ($conn->connect_error) {
+			//Fallback if unable to connect to database
+			include_once ('./php/error.php');
+			exit();
+		}
+
+		$query = "SELECT p.id, p.name, p.price, p.discount, COUNT(*) AS numberOfTimesBought
+		          FROM products AS p,
+		               inventory AS i,
+		               orders_inventory AS oi
+		          WHERE i.id = oi.inventoryIDAND p.id = i.productsID
+		          GROUP BY(p.id)
+		          ORDER BY numberOfTimesBought
+		          LIMIT 0,4;";
+
+		$result = $conn->query($query);
+
+		/**
+		 * Generate from database recursively
+		 */
+		if ($result) {
+			$num_rows = $result->num_rows;
+			if ($num_rows > 0) {
+				for ($i = 0; $i < $num_rows; $i++) {
+					$row = $result->fetch_assoc();
+					$product_id   = $row["id"];
+					$product_name = $row["name"];
+					echo '
+						<div>
+							<span>' . $product_name . '</span>
+						</div>';
+				}
+			}
+		}
+
+		/**
+		 * isset check for superglobals
+		 */
+		if(isset($_POST["mail"]) && !empty($_POST["mail"])) {
+			echo "Yes, mail is set";    
+		} else{
+			echo "N0, mail is not set";
+		}
+
+	?>
+
+	<?php include './external.php' ?>
+
+	<!-- JS -->
+
+	<script type="text/javascript">
+		var regex = /[A-Za-z]/;
+		regex.test('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+	</script>
+
+	<script type="text/javascript" src='./js/main.js'></script>
+</body>
+</html>
---
+++ notes.md
@@ -0,0 +1,51 @@
+
+# Regex
+
+## Character classes
+.			any character except newline
+\w \d \s	word, digit, whitespace
+\W \D \S	not word, digit, whitespace
+[abc]		any of a, b, or c
+[^abc]		not a, b, or c
+[a-g]		character between a & g
+
+## Anchors
+^abc$		start / end of the string
+\b \B		word, not-word boundary
+
+## Escaped characters
+\. \* \\	escaped special characters
+\t \n \r	tab, linefeed, carriage return
+\u00A9		unicode escaped ©
+
+## Groups & Lookaround
+(abc)		capture group
+\1			backreference to group #1
+(?:abc)		non-capturing group
+(?=abc)		positive lookahead
+(?!abc)		negative lookahead
+
+## Quantifiers & Alternation
+a* a+ a?	0 or more, 1 or more, 0 or 1
+a{5} a{2,}	exactly five, two or more
+a{1,3}		between one & three
+a+? a{2,}?	match as few as possible
+ab|cd		match ab or cd
+
+# HTML/CSS
+
+## Alignment
+margin: auto;
+
+### Flex
+
+display: flex;
+flex-wrap: nowrap | wrap | wrap-reverse;
+flex-grow: number;
+justify-content: flex-start | flex-end | center | space-between | space-around | space-evenly;
+align-content: flex-start | flex-end | center | space-between | space-around | stretch;
+align-items: flex-start | flex-end | center | baseline | stretch;
+align-self: auto | flex-start | flex-end | center | baseline | stretch;
+
+align-items		-> 1 line
+align-content	-> multiline
